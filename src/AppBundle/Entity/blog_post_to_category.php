0bab785d50df071e14fd0e12cb702464181f68a0
Mariano Heller <marianoheller91@gmail.com>
2016-04-22 13:19:24 -0300

    Added blog_post_to_category entity. Gotta finish mapping.

---
+++ src/AppBundle/Entity/blog_post_to_category.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * blog_post_to_category
+ *
+ * @ORM\Table(name="blog_post_to_category")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\blog_post_to_categoryRepository")
+ */
+class blog_post_to_category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="category_id", type="integer")
+     */
+    private $categoryId;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="post_id", type="integer")
+     */
+    private $postId;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set categoryId
+     *
+     * @param integer $categoryId
+     *
+     * @return blog_post_to_category
+     */
+    public function setCategoryId($categoryId)
+    {
+        $this->categoryId = $categoryId;
+
+        return $this;
+    }
+
+    /**
+     * Get categoryId
+     *
+     * @return int
+     */
+    public function getCategoryId()
+    {
+        return $this->categoryId;
+    }
+
+    /**
+     * Set postId
+     *
+     * @param integer $postId
+     *
+     * @return blog_post_to_category
+     */
+    public function setPostId($postId)
+    {
+        $this->postId = $postId;
+
+        return $this;
+    }
+
+    /**
+     * Get postId
+     *
+     * @return int
+     */
+    public function getPostId()
+    {
+        return $this->postId;
+    }
+}
+
