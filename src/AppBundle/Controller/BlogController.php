1eebbc657a9b0119a5e2599927ace600be0ad7e1
Mariano Heller <marianoheller91@gmail.com>
2016-04-22 18:14:55 -0300

    Changed controller.

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -3,6 +3,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends Controller
@@ -17,4 +18,15 @@ class BlogController extends Controller
     {
         return $this->render('blog/blog_content..html.twig');
     }
+
+    /**
+     * @Route( "cmd/init",
+     *     name="init"
+     *      )
+     */
+    public function initAction()
+    {
+        return new Response("Blog inicializado");
+    }
+
 }
