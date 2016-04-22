8f0da64c3f0384206a1b0d2c938f1d5a177c4a70
Mariano Heller <marianoheller91@gmail.com>
2016-04-22 16:56:47 -0300

    Started working with templates and created minimal controller.

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -1,18 +1,20 @@
 <?php
-
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class BlogController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/blog",
+     *     name="blog"
+     *
+     *     )
      */
     public function indexAction(Request $request)
     {
-        return $this->redirectToRoute("blog");
+        return $this->render('blog/blog_content..html.twig');
     }
 }
