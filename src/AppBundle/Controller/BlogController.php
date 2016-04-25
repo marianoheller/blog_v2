0c0348794b0799bc4c106a8df8b157c7f471f64b
Mariano Heller <marianoheller91@gmail.com>
2016-04-25 14:20:12 -0300

    Controller functions. Problem with  framework folder.

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -6,6 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\CssSelector\CssSelectorConverter;
+
+use AppBundle\Entity\blog_author;
+use AppBundle\Entity\blog_post;
+
+
+const imagesFolderName = "images_blog";
+const cantPostsAtInit = 10;
+define ("Authors", serialize (array ("Jesse Stokes", "Mike Myers", "Micky Vainilla","Skyler Johansson")));
+
+
 class BlogController extends Controller
 {
     /**
@@ -29,4 +42,36 @@ class BlogController extends Controller
         return new Response("Blog inicializado");
     }
 
+    //********************************************************************************************
+    // Private functions
+    //********************************************************************************************
+
+    private function generateAuthor( $index )
+    {
+        $userUrlApi = "http://uinames.com/api/";
+
+        $authors = unserialize(Authors);
+        $fullName = $authors[$index];
+        $nameArray = explode(" ",$fullName);
+        $lastName = "Doe";
+        if ( sizeof($nameArray) > 1 )
+            $lastName = $nameArray[1];
+        $name = $nameArray[0];
+
+
+        $author = new blog_author();
+        $author->setFirstName($name);
+        $author->setMail(strtolower($name)."@gmail.com");
+        $author->setLastName($lastName);
+
+        $author->setDisplayName($name);
+
+        return $author;
+    }
+
+    private function generatePost()
+    {
+        $post = new blog_post();
+        return $post;
+    }
 }
